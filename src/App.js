69fb838cd2cadf7d05a2e9076bf5de5c4bc7761e
smoumita19 <smoumita19@yahoo.com>
2023-01-11 20:09:01 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './components/HomescreenComponent/HomeScreen';
+import Login from './components/LoginComponent/Login'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+
 
 function App() {
+
+  const user = null;
+  // const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  console.log(useSelector(selectUser));
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if(userAuth) {
+        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email
+          })
+        )
+      } else {
+        //Logged off
+        dispatch(logout);
+      }
+    });
+    return unsubscribe;
+  },[]);
+
+
   return (
     <div className="app">
-      {/*Home Screen*/}
-      <HomeScreen />
+
+   
+      <Router>
+
+        {!user ? (
+          <Login />
+
+        ) : (
+          <Switch>
+            <Route exact path="/">
+                {/*Home Screen*/}
+                <HomeScreen />
+            </Route>
+          </Switch>
+        )
+        
+        
+        }
+
+    
+
+      </Router>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,14 @@
+// import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';        // for authentication
+import 'firebase/storage';     // for storage
+import 'firebase/database';    // for realtime database
+import 'firebase/firestore';   // for cloud firestore
+import 'firebase/messaging';   // for cloud messaging
+import 'firebase/functions';   // for cloud functions
+// import 'firebase/<PACKAGE>';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyDYFXYYnfYYIQ0XeHlBpnTV019DMoBNAy4",
     authDomain: "netflix-clone-3efc8.firebaseapp.com",
@@ -6,3 +17,14 @@ const firebaseConfig = {
     messagingSenderId: "585888328691",
     appId: "1:585888328691:web:28f6e467bfd3f250dc4086"
   };
+
+//initializing firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+//initializing database
+const db = firebaseApp.firestore(); //write down about firestore
+//authentication
+const auth = firebase.auth();
+
+//Can have one default export but many explicit export
+export { auth };
+export default db;
